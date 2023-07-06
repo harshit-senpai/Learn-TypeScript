d1d96b1b42f5525fa8e56460fdec12a5bd01e88f
Harshit Mishra <miananya2@gmail.com>
2023-07-07 02:00:26 +0530

    first app

--- getting-started-with-TypeScript/firstApp.ts
+++ getting-started-with-TypeScript/firstApp.ts
@@ -2,16 +2,26 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(response => {
-    const todo =  response.data;
+    const todo = response.data as Todo;
+    
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
 
-    const ID = todo.ID;
-    const title = todo.Title;
-    const finished = todo.Finished;
+    logTodo(id, title, completed);
+});
 
+const logTodo = (id:number, title:string, completed:boolean) => {
     console.log(`
-        The Todo with ID: ${ID}
+        The Todo with ID: ${id}
         Has a title of: ${title}
-        Is it Finished? ${finished}
-    `)
-})
+        is itt finished: ${completed}
+    `);
+};
